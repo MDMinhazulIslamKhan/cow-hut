6bfa9dd2e00f77e2051083d7d4639c981b99bfbc
MDMinhazulIslamKhan <113751180+MDMinhazulIslamKhan@users.noreply.github.com>
2023-06-19 15:38:14 +0600

    feat: user and cow all feather

--- dist/app/modules/user/user.model.js
+++ dist/app/modules/user/user.model.js
@@ -1,47 +1,50 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const user_constant_1 = require("./user.constant");
-const userSchema = new mongoose_1.Schema({
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const mongoose_1 = require('mongoose');
+const user_constant_1 = require('./user.constant');
+const userSchema = new mongoose_1.Schema(
+  {
     phoneNumber: {
-        type: String,
-        required: true,
-        unique: true,
+      type: String,
+      required: true,
+      unique: true,
     },
     role: {
-        type: String,
-        required: true,
-        enum: user_constant_1.userRoles,
+      type: String,
+      required: true,
+      enum: user_constant_1.userRoles,
     },
     password: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     name: {
-        firstName: {
-            type: String,
-            required: true,
-        },
-        lastName: {
-            type: String,
-            required: true,
-        },
-    },
-    address: {
+      firstName: {
         type: String,
         required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+    },
+    address: {
+      type: String,
+      required: true,
     },
     budget: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
     income: {
-        type: Number,
-        required: true,
+      type: Number,
+      required: true,
     },
-}, {
+  },
+  {
     timestamps: true,
-});
+  }
+);
 userSchema.index({ phoneNumber: 1 }, { unique: true });
 const User = (0, mongoose_1.model)('User', userSchema);
 exports.default = User;
